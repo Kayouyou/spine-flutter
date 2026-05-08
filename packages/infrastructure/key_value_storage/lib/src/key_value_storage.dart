92d2a73d69a859092fbdbaae46b94687876f5c55
Kayouyou <yeyangyang519716@gmail.com>
2026-05-08 16:08:20 +0800

    fix: KeyValueStorage 多类型存储 Hive 崩溃
    _openBox<T> 用不同泛型打开同 box "default" 导致 Hive 抛异常。
    改为打开 Box<dynamic>，返回值在各方法内 as 转型。

--- packages/infrastructure/key_value_storage/lib/src/key_value_storage.dart
+++ packages/infrastructure/key_value_storage/lib/src/key_value_storage.dart
@@ -9,47 +9,47 @@ class KeyValueStorage {
 
   final HiveInterface _hive;
 
-  Future<Box<T>> _openBox<T>(String key) async {
+  Future<Box> _openBox(String key) async {
     if (_hive.isBoxOpen(key)) {
-      return _hive.box(key) as Box<T>;
+      return _hive.box(key);
     }
     final appDir = await getApplicationDocumentsDirectory();
     Hive.init(p.join(appDir.path, 'hive'));
-    return _hive.openBox<T>(key);
+    return _hive.openBox(key);
   }
 
   Future<void> putString(String key, String value) async {
-    final box = await _openBox<String>(_defaultBoxKey);
+    final box = await _openBox(_defaultBoxKey);
     await box.put(key, value);
   }
 
   Future<String?> getString(String key) async {
-    final box = await _openBox<String>(_defaultBoxKey);
-    return box.get(key);
+    final box = await _openBox(_defaultBoxKey);
+    return box.get(key) as String?;
   }
 
   Future<void> putInt(String key, int value) async {
-    final box = await _openBox<int>(_defaultBoxKey);
+    final box = await _openBox(_defaultBoxKey);
     await box.put(key, value);
   }
 
   Future<int?> getInt(String key) async {
-    final box = await _openBox<int>(_defaultBoxKey);
-    return box.get(key);
+    final box = await _openBox(_defaultBoxKey);
+    return box.get(key) as int?;
   }
 
   Future<void> putBool(String key, bool value) async {
-    final box = await _openBox<bool>(_defaultBoxKey);
+    final box = await _openBox(_defaultBoxKey);
     await box.put(key, value);
   }
 
   Future<bool?> getBool(String key) async {
-    final box = await _openBox<bool>(_defaultBoxKey);
-    return box.get(key);
+    final box = await _openBox(_defaultBoxKey);
+    return box.get(key) as bool?;
   }
 
   Future<void> delete(String key) async {
-    final box = await _openBox<String>(_defaultBoxKey);
+    final box = await _openBox(_defaultBoxKey);
     await box.delete(key);
   }
 }
