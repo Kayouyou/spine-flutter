49bdd0e89370e8ccba987532186c8608ca46fb95
Kayouyou <13092497+Kayouyou@users.noreply.github.com>
2026-06-08 10:53:35 +0800

    test(app): cover AppBlocObserver.onError reporting to AppErrorHandler

---
+++ lib/core/bloc/app_bloc_observer_test.dart
@@ -0,0 +1,56 @@
+// lib/core/bloc/app_bloc_observer_test.dart
+import 'package:bloc/bloc.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:error/error.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'app_bloc_observer.dart';
+
+class _TestCubit extends Cubit<int> {
+  _TestCubit() : super(0);
+  void bang() => throw StateError('boom from cubit');
+}
+
+class _NullReporter implements ErrorReporter {
+  final List<Object> errors = [];
+  final List<StackTrace?> stacks = [];
+  final List<Map<String, dynamic>?> contexts = [];
+
+  @override
+  Future<void> reportError(
+    Object error,
+    StackTrace? stack, {
+    bool isFatal = false,
+    Map<String, dynamic>? context,
+  }) async {
+    errors.add(error);
+    stacks.add(stack);
+    contexts.add(context);
+  }
+}
+
+void main() {
+  group('AppBlocObserver.onError', () {
+    late _NullReporter reporter;
+
+    setUp(() {
+      reporter = _NullReporter();
+      AppErrorHandler.instance.setReporter(reporter);
+    });
+
+    test('forwards cubit error with bloc/source context', () {
+      final observer = AppBlocObserver();
+      final cubit = _TestCubit();
+      final stack = StackTrace.current;
+
+      observer.onError(cubit, StateError('boom from cubit'), stack);
+
+      expect(reporter.errors, hasLength(1));
+      expect(reporter.errors.first, isA<StateError>());
+      expect(reporter.stacks.first, same(stack));
+      expect(reporter.contexts.first, {
+        'source': 'bloc',
+        'bloc': '_TestCubit',
+      });
+    });
+  });
+}
